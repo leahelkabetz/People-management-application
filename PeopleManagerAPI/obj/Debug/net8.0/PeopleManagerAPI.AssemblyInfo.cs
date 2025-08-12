4dddd28cf5b32f63cdf07b76a3b658c416054dda
leahelkabetz <leah23531@gmail.com>
2025-08-12 22:57:19 +0300

    Changing MDF file location

--- PeopleManagerAPI/obj/Debug/net8.0/PeopleManagerAPI.AssemblyInfo.cs
+++ PeopleManagerAPI/obj/Debug/net8.0/PeopleManagerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeopleManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e251200e976769d6a135467ae112a7488e6152")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeopleManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeopleManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
